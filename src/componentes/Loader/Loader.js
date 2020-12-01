d5f68a97e54ed95fb0d9ab1e0113979061f7398d
German Mora <german.mora.664@gmail.com>
2020-12-01 14:39:13 -0300

    correcciones

--- src/componentes/Loader/Loader.js
+++ src/componentes/Loader/Loader.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './loader.css'
 const Loader = () => {
     return (
         <div className="d-flex justify-content-center mt-5">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './bootstrap.min.css';
 import 'bootstrap';
