0fe6c2ac16eae8fc08a88820f5c5b7c4bfce0bb6
German Mora <german.mora.664@gmail.com>
2020-12-01 21:42:58 -0300

    corrección order search

--- src/componentes/Orders/OrderDetail.js
+++ src/componentes/Orders/OrderDetail.js
@@ -7,7 +7,7 @@ import ErrorMessage from '../Message/ErrorMessage'
 
 const OrderDetail = () => {
     const { orderId } = useParams()
-    const [order, setOrder] = useState()
+    const [order, setOrder] = useState("")
     const [errorMessage, setErrorMessage] = useState("")
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
@@ -46,66 +46,67 @@ const OrderDetail = () => {
                 error ? <div>   <Link to="/"><span className="btn btn-secondary my-3">Atrás</span></Link><div className="d-flex justify-content-center "><ErrorMessage>{errorMessage}</ErrorMessage></div></div>
                     :
                     <Row className="justify-content-center">
+                        {order &&
+                            <>
+                                {success &&
+                                    <Col md={4}>
+                                        <ListGroup variant="shadow">
+                                            <ListGroup.Item className="px-2 ">
+                                                <h3>Información de entrega</h3>
+                                            </ListGroup.Item>
+                                            <ListGroup.Item>
+                                                <p>
+                                                    <strong> Nombre: </strong>
+                                                    {order.buyer.name}
+                                                </p>
+                                                <p>
+                                                    <strong> Teléfono: </strong>
+                                                    {order.buyer.phone}
+                                                </p>
+                                                <p>
+                                                    <strong> Dirección: </strong>
+                                                    {order.buyer.address}
+                                                </p>
+                                                <p className="text-center mt-4">
 
-                        <>
-                            {success &&
-                                <Col md={4}>
-                                    <ListGroup variant="shadow">
-                                        <ListGroup.Item className="px-2 ">
-                                            <h3>Información de entrega</h3>
-                                        </ListGroup.Item>
+                                                    {order.delivered ? <span className="border border-success shadow-sm p-2 rounded-lg">Entregado</span> : <span className="border border-danger shadow-sm p-2 rounded-lg">No entregado</span>}
+                                                </p>
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </Col>
+                                }
+                                <Col md={8}>
+
+                                    <ListGroup variant="flush">
                                         <ListGroup.Item>
-                                            <p>
-                                                <strong> Nombre: </strong>
-                                                {order.buyer.name}
-                                            </p>
-                                            <p>
-                                                <strong> Teléfono: </strong>
-                                                {order.buyer.phone}
-                                            </p>
-                                            <p>
-                                                <strong> Dirección: </strong>
-                                                {order.buyer.address}
-                                            </p>
-                                            <p className="text-center mt-4">
+                                            <h2>Orden: {orderId}</h2>
+                                            <h5>Creada el: {order.date.toDate().toLocaleDateString()}</h5>
+                                        </ListGroup.Item>
 
-                                                {order.delivered ? <span className="border border-success shadow-sm p-2 rounded-lg">Entregado</span> : <span className="border border-danger shadow-sm p-2 rounded-lg">No entregado</span>}
-                                            </p>
+                                        {order.items.map((item) => {
+                                            return <ListGroup.Item key={item.product}>
+                                                <Row className="align-items-center text-center">
+                                                    <Col md={2}>
+                                                        <Image src={item.image} alt={item.title} fluid rounded />
+                                                    </Col>
+                                                    <Col md={4} >
+                                                        <strong>{item.title}</strong>
+                                                    </Col>
+                                                    <Col md={4}>
+                                                        {item.quantity} x {item.price} = ${formatNumber(item.quantity * item.price)}
+                                                    </Col>
+                                                    <Col md={2}>
+                                                        <Link to={`/item/${item.id}`}><Button className="btn btn-success"> Volver a comprar</Button> </Link> </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                        })}
+                                        <ListGroup.Item>
+                                            <h3 className="text-center mt-3 ">TOTAL: ${formatNumber(order.total)}</h3>
                                         </ListGroup.Item>
                                     </ListGroup>
                                 </Col>
-                            }
-                            <Col md={8}>
-
-                                <ListGroup variant="flush">
-                                    <ListGroup.Item>
-                                        <h2>Orden: {orderId}</h2>
-                                        <h5>Creada el: {order.date.toDate().toLocaleDateString()}</h5>
-                                    </ListGroup.Item>
-
-                                    {order.items.map((item) => {
-                                        return <ListGroup.Item key={item.product}>
-                                            <Row className="align-items-center text-center">
-                                                <Col md={2}>
-                                                    <Image src={item.image} alt={item.title} fluid rounded />
-                                                </Col>
-                                                <Col md={4} >
-                                                    <strong>{item.title}</strong>
-                                                </Col>
-                                                <Col md={4}>
-                                                    {item.quantity} x {item.price} = ${formatNumber(item.quantity * item.price)}
-                                                </Col>
-                                                <Col md={2}>
-                                                    <Link to={`/item/${item.id}`}><Button className="btn btn-success"> Volver a comprar</Button> </Link> </Col>
-                                            </Row>
-                                        </ListGroup.Item>
-                                    })}
-                                    <ListGroup.Item>
-                                        <h3 className="text-center mt-3 ">TOTAL: ${formatNumber(order.total)}</h3>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </Col>
-                        </>
+                            </>
+                        }
                     </Row>
             }
         </Container >
